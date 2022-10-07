d979ec33a787497819cb89ff541d0c1ba071e735
tereesaalvarez <alvdeporteresa@gmail.com>
2022-10-07 16:31:01 +0200

    commit

--- Evaluacion4/Ejercicio5.py
+++ Evaluacion4/Ejercicio5.py
@@ -15,3 +15,23 @@ class vehiculo():
         self.ruedas = ruedas
     def get_ruedas(self):
         return self.ruedas
+
+class coche(vehiculo):
+    def __init__(self, color, ruedas, velocidad, cilindrada):
+        self.color = color
+        self.ruedas = ruedas
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+
+    def __str__(self):
+        return "Color {}, {}ruedas ".format(self.color, self.ruedas)
+
+    def set_color(self, color):
+        self.color = color
+    def get_color(self):
+        return self.color
+
+    def set_ruedas(self, ruedas):
+        self.ruedas = ruedas
+    def get_ruedas(self):
+        return self.ruedas
