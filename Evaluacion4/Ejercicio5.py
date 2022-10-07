ba8d07d72461f2e3c47905667c789afc8203ed35
tereesaalvarez <alvdeporteresa@gmail.com>
2022-10-07 16:55:02 +0200

    commit

--- Evaluacion4/Ejercicio5.py
+++ Evaluacion4/Ejercicio5.py
@@ -42,4 +42,41 @@ class bicicleta(vehiculo):
         self.tipo = tipo
     
     def __str__(self):
-        return super().__str__() + ""
+        return super().__str__() + ", {}urbana/deportiva".format(self.tipo)
+    
+    def set_tipo(self, tipo):
+        self.tipo = tipo
+    def get_tipo(self):
+        return self.tipo
+
+class camioneta(vehiculo):
+    def __init__(self,color,ruedas,carga):
+        super().__init__(color,ruedas)
+        self.carga = carga
+    def __str__(self):
+        return super().__str__() + ", {} kg".format(self.carga)
+
+    def set_carga(self, carga):
+        self.carga=carga
+    def get_carga(self):
+        return self.carga
+
+class moto(vehiculo):
+    def __init__(self, color, ruedas, velocidad, cilindrada):
+        #uso el super para traer lo anterior
+        super().__init__(color,ruedas)
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+
+    def __str__(self):
+        return super().__str__() + ", {} km/h, {} cc".format(self.velocidad, self.cilindrada)
+
+    def set_velocidad(self, velocidad):
+        self.velocidad = velocidad
+    def get_velocidad(self):
+        return self.velocidad
+
+    def set_cilindrada(self, cilindrada):
+        self.cilindrada = cilindrada
+    def get_cilindrada(self):
+        return self.cilindrada
