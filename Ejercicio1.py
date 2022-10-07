9ff63ebb5d16996d3f914ebfdb0a1bb3bcc114c7
tereesaalvarez <alvdeporteresa@gmail.com>
2022-10-07 15:43:38 +0200

    commit

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -3,3 +3,8 @@ class alumno():
         self.alumno = alumno
         self.nota = nota
         print("El alumno se ha creado con éxito")
+
+    def calificacion():
+        nota= input("Introduce la nota del alumno: ")
+        if nota>= 5 and nota <=10:
+            print("")
